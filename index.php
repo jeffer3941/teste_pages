21c745ca1540a49d91347374c160d4dd4b18a5f1
jeffer3941 <jeffersonferreira3941@gmail.com>
2021-01-25 20:09:50 -0300

    sem sqlinjection

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php 
-    $dsn = 'mysql:host=localhost;dbname=estudo_pdo';
-    $usuario = 'root';
-    $senha = '';
+    const DSN = 'mysql:host=localhost;dbname=estudo_pdo';
+    const USUARIO  = 'root';
+    const SENHA = '';
 
 
-    if(is_numeric($_POST['idade']) && is_string($_POST['nome']) || !empty($_POST['idade'] || !empty($_POST['nome']))  ) {
+    if( is_string($_POST['nome'])  ) {
         try {
-            $conexao = new PDO($dsn, $usuario, $senha);    
+            $conexao = new PDO(DSN,USUARIO, SENHA );    
             
-            $query = "select * from usuarios where nome_usuario like('%{$_POST['nome']}%') and idade_usuario like({$_POST['idade']})";
-
-            $stmt = $conexao->query($query);
-            $lista = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            echo $lista['nome_usuario'];
             
+            $query = "select * from usuarios where nome_usuario like(:nome) and idade_usuario = :idade";
+           
+            $stmt = $conexao->prepare($query);
+            echo $_POST['idade'];
+            $stmt->bindValue(':nome' , $_POST['nome']);
+            $stmt->bindValue(':idade' , $_POST['idade'], PDO::PARAM_INT);
+            
+            $stmt->execute();
+
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+            echo '<pre>';
+            print_r($resultado);
+            echo '</pre>';
+
         } catch (PDOException $e) {
             echo '<h1>';
             echo 'ATENCAO! CONTATE O SUPORTE:';
